6a05acb645a02bc404295780630e6d608c1170d0
R4chel <rae84@cornell.edu>
2022-08-17 11:54:19 -0400

    clean up

--- config.js
+++ config.js
@@ -3,13 +3,11 @@ function Config({
     debug,
     canvasWidth,
     canvasHeight,
-    threshold,
 }) {
 
     this.debug = debug === undefined ? false : debug;
     this.canvasWidth = canvasWidth === undefined ? (debug ? 200 : windowWidth) : canvasWidth;
     this.canvasHeight = canvasHeight === undefined ? (debug ? 200 : windowHeight) : canvasHeight;
-    this.threshold = threshold === undefined ? 0.4 : threshold;
     this.seed = seed === undefined ? (debug ? 1 : seed) : seed;
     this.setSeed = function() {
         if (this.seed === undefined) {
@@ -17,14 +15,5 @@ function Config({
         }
         randomSeed(this.seed);
     };
-
-    this.getThreshold = function() {
-        return this.threshold;
-    };
-
-    this.setThreshold = function(threshold) {
-        this.threshold = threshold;
-    };
-
 
 }
--- sketch.js
+++ sketch.js
@@ -4,10 +4,6 @@ let mic, fft, amplitude;
 let ranges = ["bass", "lowMid", "mid", "highMid", "treble"];
 let frequencies = new Object();
 
-let beat;
-let lastPeak;
-
-
 let seed;
 // seed = 0;
 let canvasSize;
@@ -25,10 +21,8 @@ function setup() {
     canvas.mouseClicked(canvasMouseClicked);
 
 
-    makeSlider("threshold", 0, 1,0.05,config.getThreshold,config.setThreshold);
     art = new Art(config, ranges);
     art.reset();
-
 
     let smoothing = 0.8;
     mic = new p5.AudioIn();
@@ -40,10 +34,6 @@ function setup() {
     // amplitude.toggleNormalize(true);
 
     mic.start();
-
-    beat = millis();
-    lastPeak = 0;
-
 }
 
 function process_update(data) {
@@ -86,14 +76,9 @@ function canvasMouseClicked() {
 }
 
 function draw() {
-
-    // console.log("level", mic.getLevel());
-    // console.log("seed", seed);
-
     let spectrum = fft.analyze();
     let soundwave = fft.waveform();
 
-    // let amplitudeLevel = amplitude.getLevel();
     let amplitudeLevel = mic.getLevel();
 
     for (let i = 0; i < ranges.length; i++) {
@@ -101,71 +86,8 @@ function draw() {
         frequencies[range] = fft.getEnergy(range);
     }
 
-    // background(map(1 / pow((millis() - beat) / 1000 + 1, 3), 1, 0, 255, 100));
-    drawSpectrumGraph(0, 0, width, height); 
-
     art.draw(soundwave, amplitudeLevel, frequencies);
     art.update(amplitudeLevel, frequencies);
-}
-
-// https: //stackoverflow.com/questions/67726950/how-do-i-animate-this-image-to-match-with-a-bpm-in-p5-js
-let avgWindow = 20;
-let i = 0;                      // 
-function drawSpectrumGraph(left, top, w, h) {
-
-    let spectrum = fft.analyze();
-
-    stroke('limegreen');
-    fill('darkgreen');
-    strokeWeight(1);
-
-
-    let peak = 0;
-    // compute a running average of values to avoid very
-    // localized energy from triggering a beat.
-    let runningAvg = 0;
-    for (let i = 0; i < spectrum.length; i++) {
-        vertex(
-            //left + map(i, 0, spectrum.length, 0, w),
-            // Distribute the spectrum values on a logarithmic scale
-            // We do this because as you go higher in the spectrum
-            // the same perceptible difference in tone requires a 
-            // much larger chang in frequency.
-            left + map(log(i), 0, log(spectrum.length), 0, w),
-            // Spectrum values range from 0 to 255
-            top + map(spectrum[i], 0, 255, h, 0)
-        );
-
-        runningAvg += spectrum[i] / avgWindow;
-        if (i >= avgWindow) {
-            runningAvg -= spectrum[i] / avgWindow;
-        }
-        if (runningAvg > peak) {
-            peak = runningAvg;
-        }
-    }
-
-    // any time there is a sudden increase in peak energy, call that a beat
-    if (peak > lastPeak * (1 + config.threshold)) {
-        print(`tick ${++i}`);
-        circle(i, peak, 100);
-        beat = millis();
-    }
-    lastPeak = peak;
-
-    // this is the range of frequencies covered by the FFT
-    let nyquist = 22050;
-
-    // get the centroid (value in hz)
-    let centroid = fft.getCentroid();
-
-    // the mean_freq_index calculation is for the display.
-    // centroid frequency / hz per bucket
-    let mean_freq_index = centroid / (nyquist / spectrum.length);
-    stroke('red');
-    // convert index to x value using a logarithmic x axis
-    let cx = map(log(mean_freq_index), 0, log(spectrum.length), 0, width);
-    circle(cx, i*20, 5);
 }
 
 // This is a fix for chrome:
@@ -174,29 +96,4 @@ function touchStarted() {
     if (getAudioContext().state !== 'running') {
         getAudioContext().resume();
     }
-}
-
-function makeSlider(name, minimum, maximum, delta, getter, setter) {
-    let d = createDiv();
-
-    let label = createElement("label");
-    let textBox = createInput((getter.apply(config)).toString(), "number");
-    textBox.style("width", "100px");
-    textBox.attribute("step", delta);
-    let slider = createSlider(minimum, maximum, getter.apply(config), delta);
-    label.html(name);
-    label.attribute("for", slider.id());
-    slider.input(function() {
-        setter.apply(config, [slider.value()]);
-        textBox.value(slider.value());
-    });
-    textBox.input(function() {
-        let value = parseFloat(textBox.value());
-        setter.apply(config, [value]);
-        slider.value(value);
-    });
-    d.child(label);
-    d.child(slider);
-    d.child(textBox);
-    return slider;
 }
