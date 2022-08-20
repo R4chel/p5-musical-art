dc47b9c0bc78c319843601aac7dcfe7b535b67cc
R4chel <rae84@cornell.edu>
2022-08-19 20:34:26 -0400

    add frequency band file

---
+++ frequencyBand.js
@@ -0,0 +1,18 @@
+function FrequencyBand({
+    minValue, maxValue, framesForAction
+}){
+    this.minValue = minValue;
+    this.maxValue = maxValue;
+    this.framesForAction = framesForAction;
+    this.frames = 0;
+
+    this.update = function(frequency){
+        if(frequency => this.minValue && frequency <= this.maxValue){
+            this.frames++;
+        }
+        else{
+            this.frames = 0;
+        }
+        return (this.frames > this.framesForAction);
+    };
+}
